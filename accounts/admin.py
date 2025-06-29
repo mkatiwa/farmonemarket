5acc012eca12d8341511ca3ff1512496a6d84c21
mkatiwa <m.katiwa@alustudent.com>
2025-06-29 16:54:27 +0300

    db

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
+from . import models
 from accounts.models import User
 
-# Register your models here.
-admin.site.register(User)
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'role', 'phone']
+    list_editable = ['role', 'email', 'phone' ]
+    list_display_links = None
--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from carts.models import Cart
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at', 'updated_at' ]
